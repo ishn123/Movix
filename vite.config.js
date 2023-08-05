bc5ed33b5236d4124f717ed244724ffe8e5b6c7f
ishn123 <62070580+ishn123@users.noreply.github.com>
2023-08-05 16:13:13 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server:{
-    host:"192.168.0.101"
-  }
+  plugins: [react()]
 })
